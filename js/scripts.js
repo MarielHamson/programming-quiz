cf33991aa35214790e49e9b6769beb4f545b1014
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-05 10:42:55 -0700

    add logic to collect responses from quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic
 
-const add = function(question1, question2, question3, question4, question5) {
+const add = function(question1, question2, question4, question5) {
   return question1 + question2 + question3 + question4 + question5
 }
 
@@ -10,4 +10,16 @@ $(document).ready(function() {
     $("#quiz").show();
     event.preventDefault();
   });
+// User Interface Logic
+
+$("form#quiz").submit(function(event) {
+  event.preventDefault()
+  const question1 = parseInt($("#hero").val());
+  const question2 = parseInt($("input:radio[name=game]:checked").val());
+  const question4 = parseInt($("input:radio[name=captain]:checked").val());
+  const question5 = parseInt($("#tp").val());
+  console.log(firstName, question1, question2, question4, question5);
+
+});
+
 });
