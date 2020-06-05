555fb073750948c2fd8dff0a14188cf7ec2d44f1
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-05 13:13:35 -0700

    add modal for results with styling

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,13 @@ $("form#quiz").submit(function(event) {
   let result = add(question1, question2, question4, question5);
 
   if (result >= 6 && result <= 10) {
-    $(".langResult").text("Ruby" + ", " + firstName);
+    $("#langResult").text("Ruby" + ", " + firstName);
   } else if (result >=11 && result <= 15) {
-    $(".langResult").text("Javascript" + ", " + firstName);
+    $("#langResult").text("Javascript" + ", " + firstName);
   } else if (result >= 15 && result <=20) {
-    $(".langResult").text("Python" + ", " + firstName);
+    $("#langResult").text("Python" + ", " + firstName);
   } else {
-    $(".langResult").text("How to Hang Toilet Paper", + firstName);
+    $("#langResult").text("How to Hang Toilet Paper", + firstName);
   }
-  $("#output").show();
   });
 });
