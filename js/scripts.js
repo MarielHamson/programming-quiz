8fd82ba35d938301dcfeb9de5990cb4e84a63eb7
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-05 09:57:29 -0700

    add business logic for adding up answer values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Business Logic
+
+const add = function(question1, question2, question3, question4, question5) {
+  return question1 + question2 + question3 + question4 + question5
+}
+
 $(document).ready(function() {
   $(".form-inline").submit(function(event) {
     firstName = $("input#name").val();
