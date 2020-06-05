60e3438f214499d0a98bae026077dea287794b94
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-05 15:50:53 -0700

    clean up indentation and remove unnecessary stylesheet link

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Business Logic
-
 const add = function(question1, question2, question4, question5) {
   return question1 + question2 + question4 + question5
 }
@@ -7,42 +6,42 @@ const add = function(question1, question2, question4, question5) {
 
 $(document).ready(function() {
   $(".form-inline").submit(function(event) {
-    firstName = $("input#name").val();
-    $("#q1").fadeIn();
-    event.preventDefault();
+      firstName = $("input#name").val();
+      $("#q1").fadeIn();
+      event.preventDefault();
   });
-  $("#hero").focus(function(){
-    $("#q2").fadeIn();
+  $("#hero").focus(function() {
+      $("#q2").fadeIn();
   });
-  $("[name='game']").focus(function(){
-    $("#q3").fadeIn();
+  $("[name='game']").focus(function() {
+      $("#q3").fadeIn();
   });
-  $("#color").focus(function(){
-    $("#q4").fadeIn();
+  $("#color").focus(function() {
+      $("#q4").fadeIn();
   });
   $("[name='captain']").focus(function() {
-    $("#q5").fadeIn();
-    $("body").scrollTop($("#tp").position().top)
+      $("#q5").fadeIn();
+      $("body").scrollTop($("#tp").position().top)
   });
 
 
-$("form#quiz").submit(function(event) {
-  event.preventDefault()
-  const question1 = parseInt($("#hero").val());
-  const question2 = parseInt($("input:radio[name=game]:checked").val());
-  const question4 = parseInt($("input:radio[name=captain]:checked").val());
-  const question5 = parseInt($("#tp").val());
-  console.log(firstName, question1, question2, question4, question5);
-  let result = add(question1, question2, question4, question5);
+  $("form#quiz").submit(function(event) {
+      event.preventDefault()
+      const question1 = parseInt($("#hero").val());
+      const question2 = parseInt($("input:radio[name=game]:checked").val());
+      const question4 = parseInt($("input:radio[name=captain]:checked").val());
+      const question5 = parseInt($("#tp").val());
+      console.log(firstName, question1, question2, question4, question5);
+      let result = add(question1, question2, question4, question5);
 
-  if (result >= 6 && result <= 10) {
-    $("#langResult").text(firstName + ": " + "Ruby ");
-  } else if (result >=11 && result <= 15) {
-    $("#langResult").text(firstName + ": " + "Javascript ");
-  } else if (result >= 15 && result <=20) {
-    $("#langResult").text(firstName + ": " + "Python ");
-  } else {
-    $("#langResult").text(firstName + ": " + "How to Hang Toilet Paper");
-  }
+      if (result >= 6 && result <= 10) {
+          $("#langResult").text(firstName + ": " + "Ruby ");
+      } else if (result >= 11 && result <= 15) {
+          $("#langResult").text(firstName + ": " + "Javascript ");
+      } else if (result >= 15 && result <= 20) {
+          $("#langResult").text(firstName + ": " + "Python ");
+      } else {
+          $("#langResult").text(firstName + ": " + "How to Hang Toilet Paper");
+      }
   });
 });
